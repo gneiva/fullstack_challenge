ff9f34908e471d3ab05efeaa8dc73f571fb35cc8
Gabriel Neiva <gneiva@users.noreply.github.com>
2024-05-31 21:54:25 -0300

    Adjusting for tests

--- api/src/test/java/io/github/daylanbueno/authapi/AuthApiApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package io.github.daylanbueno.authapi;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class AuthApiApplicationTests {
-
-}
---
+++ api/src/test/java/io/github/gneiva/fullstack/challenge/api/controller/AuthenticationControllerTest.java
@@ -0,0 +1,10 @@
+package io.github.gneiva.fullstack.challenge.api.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import io.github.gneiva.fullstack.challenge.api.controllers.AuthenticationController;
+
+@WebMvcTest(AuthenticationController.class)
+public class AuthenticationControllerTest {
+
+}
