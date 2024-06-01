56b296b32cda14b98edcfb3122bed8787097bc5b
Gabriel Neiva <gneiva@users.noreply.github.com>
2024-05-31 21:54:11 -0300

    Adjusting for tests

---
+++ api/src/test/java/io/github/gneiva/fullstack/challenge/api/controller/CategoryControllerTest.java
@@ -0,0 +1,10 @@
+package io.github.gneiva.fullstack.challenge.api.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import io.github.gneiva.fullstack.challenge.api.controllers.CategoryController;
+
+@WebMvcTest(CategoryController.class)
+public class CategoryControllerTest {
+
+}
